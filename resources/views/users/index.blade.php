371b442568b78d7be1d8a2991680d059abcf68a0
Vanz <zwellvvb@gmail.com>
2023-03-10 16:46:57 +0800

    VVB ongoing

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,7 +2,58 @@
 
 
 @section('content')
-<div class="row">
+{{-- <div class="d-flex justify-content-center"> --}}
+  <div class="row justify-content-center">
+    <div class="col-lg-8">
+        <div class="card">
+            <div class="card-header">
+              
+              <div class="pull-right">
+                <h2>Users Management</h2>
+                <a class="btn btn-success" href="{{ route('users.create') }}"> Create New User</a>
+              </div>
+            </div>
+            <div class="card-body">
+              <table class="table table-bordered">
+                <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Roles</th>
+                  <th width="280px">Action</th>
+                </tr>
+                @foreach ($data as $key => $user)
+                 <tr>
+                   <td>{{ ++$i }}</td>
+                   <td>{{ $user->name }}</td>
+                   <td>{{ $user->email }}</td>
+                   <td>
+                     @if(!empty($user->getRoleNames()))
+                       @foreach($user->getRoleNames() as $v)
+                          <label>{{ $v }}</label>
+                       @endforeach
+                     @endif
+                   </td>
+                   <td>
+                      <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
+
+
+                      
+                      <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
+                       {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+                           {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                       {!! Form::close() !!}
+                   </td>
+                 </tr>
+                @endforeach
+               </table>
+            </div>
+        </div>
+    </div>
+  </div>
+{{-- </div> --}}
+
+{{-- <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
             <h2>Users Management</h2>
@@ -11,7 +62,7 @@
             <a class="btn btn-success" href="{{ route('users.create') }}"> Create New User</a>
         </div>
     </div>
-</div>
+</div> --}}
 
 
 @if ($message = Session::get('success'))
@@ -21,7 +72,7 @@
 @endif
 
 
-<table class="table table-bordered">
+{{-- <table class="table table-bordered">
  <tr>
    <th>No</th>
    <th>Name</th>
@@ -50,11 +101,11 @@
     </td>
   </tr>
  @endforeach
-</table>
+</table> --}}
 
 
 {!! $data->render() !!}
 
 
-<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
+{{-- <p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p> --}}
 @endsection
